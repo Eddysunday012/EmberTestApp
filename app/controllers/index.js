03ddb2a0baeacc5a4a8d8ef6d137f526ed1be9c3
Ethan Domingo <etddomin@ucsc.edu>
2024-08-05 11:29:59 -0700

    found fix for tailwind issue + added miragejs

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,3 +1,8 @@
 import Controller from '@ember/controller';
 
-export default class IndexController extends Controller {}
+export default class IndexController extends Controller {
+  /*
+   *  Add modal logic to change what type of info is shown on the modal
+   *
+   * */
+}
